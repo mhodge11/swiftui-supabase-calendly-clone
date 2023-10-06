686490eb2f31abf9ae9207e50ab5a54d7b144a41
mhodge11 <micahhodgeutk@icloud.com>
2023-10-06 10:43:33 -0400

    Initial Commit

---
+++ SwiftUI-Supabase-Calendly-Clone/SwiftUI_Supabase_Calendly_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Supabase_Calendly_CloneApp.swift
+//  SwiftUI-Supabase-Calendly-Clone
+//
+//  Created by Micah Hodge on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Supabase_Calendly_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
